7fb3e1c5e500af80e8f3b935df7b6b9b71258583
Dell_Wyse <loks.nk@gmail.com>
2020-07-17 14:20:04 -0700

    Dell_wyse

--- Projects/rolling_dice.py
+++ Projects/rolling_dice.py
@@ -2,24 +2,22 @@ from random import randint
 
 
 def rolling_dice():
-   minimum = 1
-   maximum = 6
+  minimum = 1
+  maximum = 6
 
-   roll_dice = True
-   while roll_dice:
-       print("Rolling dice...")
-       result = randint(minimum, maximum)
-       print(result)
-       roll_dice = False
-       repeater  = input("Do you want to roll again :(yes or no)?: ").lower()
-       while repeater not in ["yes", "no"]:
-           repeater = input("Invalid input. Please try again: ").lower()
-           if repeater == 'yes':
-               roll_dice = True
-           elif repeater == "no":
-               roll_dice = False
-               print("Have a good day.")
+  roll_dice = True
+  while roll_dice:
+    print("Rolling dice...")
+    result = randint(minimum, maximum)
+    roll_dice = False
+    repeater = input("Do you want to roll again :(yes or no)?: ").lower()
+    while repeater not in ["yes", "no"]:
+      repeater = input("Invalid input. Please try again: ").lower()
+      if repeater == 'yes':
+        roll_dice = True
+      elif repeater == "no":
+        roll_dice = False
+        print("Have a good day.")
 
 
 c = rolling_dice()
-print(c)
